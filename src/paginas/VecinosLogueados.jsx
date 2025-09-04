ab8971f6161e43a7e7f5bac53a05e7ecba326c98
mariapaulavera <mariapaulaveram@gmail.com>
2025-09-04 16:51:39 -0300

    ok logueo del vecino

--- src/paginas/VecinosLogueados.jsx
+++ src/paginas/VecinosLogueados.jsx
@@ -1,5 +1,5 @@
 import {useLocation, useNavigate } from 'react-router-dom';
-import styles from '../styles/VecinosLogueados.module.css';
+import styles from '../../styles/VecinosLogueados.module.css';
 
 function VecinosLogueados() {
     const location = useLocation();
@@ -11,13 +11,16 @@ function VecinosLogueados() {
     };
 
     return (
-        <div className={styles.wrapper}>
+         <div className={styles.wrapper}>
             <div className={styles.saludoContainer}>
                 <span className={styles.saludo}>¡Bienvenido, {email}!</span>
                 <button onClick={handleLogout} className={styles.logout}>
                     Cerrar sesión
                 </button>
             </div>
+            <section id="reclamos-vecino" className={styles.presentacion}>
+                <p>Mis Reclamos</p>
+            </section>
         </div>
     );
 }
